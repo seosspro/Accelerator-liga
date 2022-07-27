6f34d069520d898d7dab25d3e3391c17bb922e5e
Дмитрий <kdimonr@mail.ru>
2022-07-27 19:13:11 +0700

    mini-fix

--- source/js/main.js
+++ source/js/main.js
@@ -3,6 +3,7 @@
 (function () {
   let navMain = document.querySelector('.main-nav');
   let navToggle = document.querySelector('.main-nav__toggle');
+  let stopScroll = document.querySelector('body');
 
   navMain.classList.remove('main-nav--nojs');
 
@@ -11,6 +12,7 @@
       if (navMain.classList.contains('main-nav--closed')) {
         navMain.classList.remove('main-nav--closed');
         navMain.classList.add('main-nav--opened');
+        stopScroll.classList.add('body--scroll');
       } else {
         navMain.classList.add('main-nav--closed');
         navMain.classList.remove('main-nav--opened');
